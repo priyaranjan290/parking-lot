d7d4cfe56a2d2906927233da8b530fff5b3e1bbc
Priyaranjan <priyaranjan@rivigo.com>
2019-08-25 14:45:44 +0530

    vehicles having duplicate registration numbers are not allowed to park

--- src/main/java/com/goJek/service/ParkingManagerImpl.java
+++ src/main/java/com/goJek/service/ParkingManagerImpl.java
@@ -4,6 +4,8 @@ import com.goJek.enums.SlotSize;
 import com.goJek.exception.ParkingException;
 import com.goJek.models.*;
 
+import java.util.List;
+
 public class ParkingManagerImpl implements ParkingManager {
 
 
@@ -30,6 +32,7 @@ public class ParkingManagerImpl implements ParkingManager {
 
     // error message to be thrown if parking lot object is null
     private static final String PARKING_LOT_IS_NULL = "Parking lot is null!";
+    private static final String DUPLICATE_VEHICLE_NOT_ALLOWED = "Duplicate Registration number vehicles are not allowed! ";
 
     /**
      *
@@ -59,16 +62,44 @@ public class ParkingManagerImpl implements ParkingManager {
 
         ParkingLot parkingLot = getParkingLot();
 
+        // check if vehicle is already parked
+        if (isDuplicateVehicle(vehicle.getRegistrationNumber(), parkingLot.getSlots())) {
+            throw new ParkingException(DUPLICATE_VEHICLE_NOT_ALLOWED);
+        }
+
+
+        // cannot park if parking lot is full
         if (parkingLot.isFull()) {
             return null;
         }
 
+        // try to park vehicle
         Slot slot = parkingLot.parkVehicle(vehicle);
 
         if (slot == null) { return null; }
 
+        // issue a ticket to client
         return ticketService.createTicket(vehicle.getRegistrationNumber(), slot.getId(), parkingLot.getId());
 
+    }
+
+    /**
+     *
+     * Iterates over all the slots to find if the vehicle of given registration number is already parked or not
+     *
+     * @param registrationNumber : registration number of the vehicle to be searched for
+     * @param slots              : list of slots to be searched
+     * @return                   : true if found else false
+     */
+    private boolean isDuplicateVehicle(String registrationNumber, List<Slot> slots) {
+        Slot duplicateSlot
+                = slots.stream()
+                        .filter(slot -> slot.isSlotOccupied() && slot.getVehicle().getRegistrationNumber().equals(registrationNumber))
+                        .findAny()
+                        .orElse(null);
+
+
+        return duplicateSlot != null;
     }
 
     /**
